b7a31c7e9eaab8cfead8d92b9862a73fe047533c
Cristiano Azevedo <crisazev10@gmail.com>
2023-06-21 10:02:39 -0400

    códigos sobre a anatomia das classes

---
+++ java-anatomia-classes/src/Metodo.java
@@ -0,0 +1,20 @@
+public class Metodo {
+
+public static void main(String[] args) {
+    
+    String n1 = "Cristiano";
+    String segundoNome = "Azevedo";
+    String terceiroNome = "de Carvalho";
+
+    String nomeCompleto = formatarNome ( n1, segundoNome, terceiroNome);
+
+    System.out.println(nomeCompleto);
+
+}
+
+public static String formatarNome ( String primeiroNome, String segundoNome, String terceiroNome){
+   // return primeiroNome.concat(" ").concat(segundoNome);
+   return "O resultado do método é: " + primeiroNome + " " + segundoNome + " " + terceiroNome;
+}
+
+}
