749282eafbabff044b5ed32730f046a13f11f6c0
Cristiano Azevedo <crisazev10@gmail.com>
2023-06-24 22:49:50 -0400

    Executando programas através do terminal

---
+++ Modulo-1/src/sintaxe/terminalArgumentos/TesteTerminal.java
@@ -0,0 +1,7 @@
+package sintaxe.terminalArgumentos;
+
+public class TesteTerminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
