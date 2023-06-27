8f51b1437264a1a0d67699dce3ca62b70d5afcd0
Cristiano Azevedo <crisazev10@gmail.com>
2023-06-26 23:31:16 -0400

    Estrutura condicional

--- Modulo-2/src/estruturasCondicionais/CondicionalEncadeada.java
+++ Modulo-2/src/estruturasCondicionais/CondicionalEncadeada.java
@@ -1,5 +1,14 @@
 package estruturasCondicionais;
 
 public class CondicionalEncadeada {
-    
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
+        else
+            System.out.println("Reprovado");
+    }    
 }
