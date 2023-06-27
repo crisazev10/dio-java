c789126efc5e09c151a613e1a38e69f46bf42950
Cristiano Azevedo <crisazev10@gmail.com>
2023-06-26 23:21:37 -0400

    Estrutura condicional

--- Modulo-2/src/estruturasCondicionais/CondicionalComposta.java
+++ Modulo-2/src/estruturasCondicionais/CondicionalComposta.java
@@ -1,5 +1,14 @@
 package estruturasCondicionais;
 
 public class CondicionalComposta {
-    
+    public static void main(String[] args) {
+
+       int nota = 6;
+       
+       if(nota >= 7)
+        System.out.println("Aprovado");
+
+       else
+        System.out.println("Reprovado");
+    }    
 }
